071d5c779fe0dee63fb46acb1faf862792ac5d90
Paolo Antinori <pantinor@redhat.com>
2017-12-06 17:05:41 +0100

    added 6a

--- src/main/java/paolo/aoc2017/dec06/A.java
+++ src/main/java/paolo/aoc2017/dec06/A.java
@@ -5,12 +5,13 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.*;
 import java.util.*;
+import java.util.function.*;
 
 public class A {
 
     public static void main(String[] args){
         try{
-        Files.readAllLines(Paths.get("/data/repositories/adventofcode2017/src/main/java/resources/06/a.txt"))
+        Files.readAllLines(Paths.get("/data/repositories/github/personal/adventofcode2017/src/main/java/resources/06/a.txt"))
         .stream()
         .forEach( line -> {
             Set<String> set = new HashSet<>();
@@ -22,9 +23,19 @@ public class A {
             while(!set.contains(str(arr))){
                 set.add(str(arr));
                 counter++;
-                IntStream st = IntStream.iterate(0, f -> f+1==arr.length?0:f+1);
-                int max = IntStream.range(0, arr.length).reduce(0, (acc, el)-> arr[el]>arr[acc]?el:acc );
-                System.out.println("max: " + max );
+                Function<Integer, IntStream> shiftedIntStreamFunction = (Integer i) -> IntStream.iterate(i, f -> f+1==arr.length?0:f+1);
+                
+                int max = IntStream.range(0, arr.length).reduce(0, (acc, el)->  arr[el]>arr[acc]?el:acc);
+
+                System.out.println("max: " + arr[max] );
+                shiftedIntStreamFunction.apply(max).limit(arr[max]+1 ).forEach(x-> {
+                    arr[max] = arr[max]-1;
+                    arr[x] = arr[x] +1;
+                });
+                System.out.println("End of step: " + str(arr));
+                    
+
+                
             }
             System.out.println("counter? "+ counter);
 
